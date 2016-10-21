9df4bbb60d953fded96c8705b41e468e467b3df7
Siddharth Pai <siddharth.pai@digifutura.com>
2016-10-21 08:31:19 +0530

    Minor Changes, CDNified

--- js/controllers/modalCtrl.js
+++ js/controllers/modalCtrl.js
@@ -1,4 +1,4 @@
-app.controller("modalCtrl",['$scope','$uibModal','$timeout',function($scope,$uibModal,$timeout){
+app.controller("modalCtrl",['$scope','$timeout',function($scope,$timeout){
   var data = [
      {team: 'Engineering', employees: ['Lawana Fan', 'Larry Rainer', 'Rahul Malik', 'Leah Shumway']},
      {team: 'Executive', employees: ['Rohan Gupta', 'Ronda Dean', 'Robby Maharaj']},
